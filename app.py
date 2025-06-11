d6d783332b13da32575a54b903df0c46e433f300
Amenasetheru <67910497+Amenasetheru@users.noreply.github.com>
2025-06-11 16:42:05 +0200

    Update app.py
    "Fix: Directly updated app.py on GitHub for Gunicorn Start Command") et cliquez sur le bouton **"Commit changes"

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from app import create_app
+from app import create_app # Importe l'usine d'application depuis le package 'app'
 
+# Crée l'instance de l'application Flask
+# Cette variable 'app' sera celle que Gunicorn cherchera.
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True) # Set debug=False for production
+    # Cette partie est UNIQUEMENT pour le développement local
+    # NE SERA PAS EXÉCUTÉE par Gunicorn sur Render
+    app.run(debug=False) # Important: Passez en debug=False pour la production
+        
 
